f2edbba28b0f329e0bbfc2dde957c145ac838318
Javkhlan <javhlan0420@gmail.com>
2024-01-05 12:20:01 +0800

    changes

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import * as fs from 'fs';
 
 export const deleteFieldsFromObjects = (filePath, fieldsToDelete) => {
-	const fileContent = fs.readFileSync(filePath, 'utf-8');
-	const jsonArray = JSON.parse(fileContent);
+	try {
+		const fileContent = fs.readFileSync(filePath, 'utf-8');
+		const jsonArray = JSON.parse(fileContent);
 
-	const modifiedArray = jsonArray.map((obj) => {
-		const newObj = {};
-		for (const key in obj) {
-			if (!fieldsToDelete.includes(key)) {
-				newObj[key] = obj[key];
+		const modifiedArray = jsonArray.map((obj) => {
+			const newObj = {};
+			for (const key in obj) {
+				if (!fieldsToDelete.includes(key)) {
+					newObj[key] = obj[key];
+				}
 			}
-		}
-		return newObj;
-	});
+			return newObj;
+		});
 
-	const jsonData = JSON.stringify(modifiedArray, null, 2);
-	fs.writeFileSync(filePath, jsonData, 'utf-8');
+		const jsonData = JSON.stringify(modifiedArray, null, 2);
+		fs.writeFileSync(filePath, jsonData, 'utf-8');
+	} catch (err) {
+		throw err;
+	}
 };
